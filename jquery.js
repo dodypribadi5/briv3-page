f77dc8459e07741aa03719e0e1f4b86877d6f703
dodypribadi5 <dodypribadi5@gmail.com>
2025-05-30 16:32:33 +0700

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,7 +1,7 @@
 let tglNow = new Date();
 let waktuBatas = new Date("2025-06-21T23:59:59");
 if (tglNow.getTime() >= waktuBatas.getTime()) {
-  document.location.href = 'https://expiredweb.pages.dev';
+  document.location.href = 'https://suspend-web.pages.dev';
 } else {
   console.log('Server aktif');
 }
